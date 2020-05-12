f7f04967f32da1f9e16d9d09d6cacae4f61f64e0
homelyseven250 <19gkeylock@kennetschool.co.uk>
2020-05-12 11:32:49 +0100

    Removed junk code

--- static/sketch.js
+++ static/sketch.js
@@ -13,9 +13,7 @@ function httpGetAsync(theUrl, callback)
 video = document.getElementById('video');
 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Not adding `{ audio: true }` since we only want video now
   navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-      //video.src = window.URL.createObjectURL(stream);
       video.srcObject = stream;
       video.play();
   });
