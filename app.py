f0b29b62c5a47d0a1542da0f1914b1443083f30d
homelyseven250 <19gkeylock@kennetschool.co.uk>
2020-05-13 09:25:31 +0100

    Added jiggle prevention beta

--- app.py
+++ app.py
@@ -8,19 +8,36 @@ def hello_world():
     #get screen size, to expand x y values
     screenWidth, screenHeight = pyautogui.size()
     #find ratio
-    Xmultiplier = float(480/screenWidth)
-    Ymultiplier = float(640/screenHeight)
+    Xmultiplier = float(screenWidth/1280)
+    Ymultiplier = float(screenHeight/720)
     
     #convert to screen size
     x = float(float(request.args["leftX"])*Xmultiplier)
     y = float(float(request.args["leftY"])*Ymultiplier)
-    #move to calculated position
-    pyautogui.moveTo(x, y)
+
+    lastLeftfile = open("lastLeft.txt", "r")
+
+    # if movement is less that 20 pixels, don't move.
+    if x - int(lastLeftfile.read()) > 21:
+        #move mouse
+        pyautogui.moveTo(x, y)
+
+    else:
+        print("Jiggle")
+
+    lastLeftfile.close()
+
+    #save last left x location
+    lastLeftfile = open("lastLeft.txt", "w")
+    lastLeftfile.write(str(x))
+    lastLeftfile.close()
+
     #########
     #REMEMBER TO RETURN!
     #########
-    return(str(x)+" "+str(y))
+    return("200 OK")
 
 @app.route('/<path:path>')
 def send_static(path):
+    #make web code availible in browser
     return send_from_directory('static', path)
--- static/sketch.js
+++ static/sketch.js
@@ -1,11 +1,7 @@
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.open("GET", theUrl, false); // true for asynchronous 
     xmlHttp.send(null);
 }
 
